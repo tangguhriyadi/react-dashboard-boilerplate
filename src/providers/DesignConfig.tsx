4642121a260010b74827b114da3e7b272cfb207b
tangguhriyadi <mtangguh97@gmail.com>
2024-08-13 21:42:25 +0700

    feat(structure): design config provider

---
+++ src/providers/DesignConfig.tsx
@@ -0,0 +1,18 @@
+import { ConfigProvider } from "antd";
+import React from "react";
+
+const DesignConfig = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <ConfigProvider
+            theme={{
+                token: {
+                    colorPrimary: "#966E32",
+                },
+            }}
+        >
+            {children}
+        </ConfigProvider>
+    );
+};
+
+export default DesignConfig;
